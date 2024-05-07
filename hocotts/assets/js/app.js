c9ed7ade69129055dfd894d581f64ce251c448c3
klmerritt44 <143474174+klmerritt44@users.noreply.github.com>
2024-05-07 00:58:32 +0000

    changed blog and lanscaping header setting up gallery page

--- hocotts/assets/js/app.js
+++ hocotts/assets/js/app.js
@@ -3,4 +3,16 @@ $( document ).ready(function() {
         $('.site-nav').toggleClass('site-nav--open', 500);
         $(this).toggleClass('open');
       })
-});
+  
+  });
+
+  $('#image-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 900,
+    fade: true,
+    slide: 'div',
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
